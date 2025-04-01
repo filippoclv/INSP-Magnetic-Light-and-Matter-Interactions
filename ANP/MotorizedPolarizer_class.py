7e30bd6d1e5e8f6830fa2bd13d6b2a615c9a296d
Filippo Calavaro <calavaro@insp.jussieu.fr>
2025-04-01 11:04:32 +0200

    adding new classes

---
+++ ANP/MotorizedPolarizer_class.py
@@ -0,0 +1,13 @@
+class MotorizedPolarizer:
+
+    def __init__(self,
+                 angle_waveplate,
+                 initial_angle,
+                 minimum_power_angle = None,
+                 maximum_power_angle = None
+                 ):
+
+        self.angle_waveplate = angle_waveplate
+        self.initial_angle = initial_angle
+
+        def
