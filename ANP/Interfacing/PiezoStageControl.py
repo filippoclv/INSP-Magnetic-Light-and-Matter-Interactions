fc9a817b105970c09f308879609ee806dcd79871
Filippo Calavaro <calavaro@insp.jussieu.fr>
2025-04-15 12:35:32 +0200

    Interfacing

--- ANP/Interfacing/PiezoStageControl.py
+++ ANP/Interfacing/PiezoStageControl.py
@@ -1,35 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 01 11:52:44 2015
-
-@author: benre
-"""
 from __future__ import print_function
 from builtins import str
 import serial
 import nplab.instrument.serial_instrument as si
 
 class Piezoconcept(si.SerialInstrument):
-    '''A simple class for the Piezo concept FOC100 nanopositioning system'''
+    """A simple class for the Piezo concept FOC100 nanopositioning system."""
     
-    def __init__(self, port=None):
+    def __init__(self, position=None, port=None):
 
+        self.position = position
         self.termination_character = '\n'
         self.port_settings = {
-                    'baudrate':115200,
-                    'bytesize':serial.EIGHTBITS,
-                    'parity':serial.PARITY_NONE,
-                    'stopbits':serial.STOPBITS_ONE,
-                    'timeout': 1, #wait at most one second for a response
-          #          'writeTimeout':1, #similarly, fail if writing takes >1s
-          #         'xonxoff':False, 'rtscts':False, 'dsrdtr':False,
+                    "baudrate":115200,
+                    "bytesize":serial.EIGHTBITS,
+                    "parity":serial.PARITY_NONE,
+                    "stopbits":serial.STOPBITS_ONE,
+                    "timeout": 1, # Wait at most one second for a response
+                    #"writeTimeout":1, #similarly, fail if writing takes >1s
+                    #"xonxoff":False, 'rtscts':False, 'dsrdtr':False,
                     }
 
-        si.SerialInstrument.__init__(self,port=port)
-        # self.recenter()
+        si.SerialInstrument.__init__(self, port=port)
+        #self.recenter()
         
-    def move_rel(self,value,unit="n"):
-        '''A command for relative movement, where the default units is nm'''
+    def move_rel(self, value, unit="n"):
+        """A command for relative movement, where the default units is nm."""
+
         if unit == "n":
 
             multiplier=1
@@ -47,13 +43,14 @@ class Piezoconcept(si.SerialInstrument):
             self.write("MOVRX "+str(value)+unit)
             self.position=(value*multiplier+self.position)
     
-    def MOVEX(self,value,unit="n"):
-        '''An absolute movement command, will print an error to the console 
-        if you moveoutside of the range(100um) default unit is nm'''
+    def MOVEX(self, value, unit="n"):
+        """An absolute movement command, will print an error to the console
+        if you move outside of the range(100um) default unit is nm"""
 
         if unit == "n":
 
             multiplier=1
+
         if unit == "u":
 
             multiplier=1E3
@@ -67,13 +64,14 @@ class Piezoconcept(si.SerialInstrument):
             self.write("MOVEX "+str(value)+unit)
             self.position = value*multiplier
 
-    def MOVEY(self,value,unit="n"):
-        '''An absolute movement command, will print an error to the console 
-        if you moveoutside of the range(100um) default unit is nm'''
+    def MOVEY(self, value, unit="n"):
+        """An absolute movement command, will print an error to the console
+        if you move outside of the range(100um) default unit is nm"""
 
         if unit == "n":
 
             multiplier=1
+
         if unit == "u":
 
             multiplier=1E3
@@ -87,12 +85,12 @@ class Piezoconcept(si.SerialInstrument):
             self.write("MOVEY "+str(value)+unit)
             self.position = value*multiplier
             
-    def move_step(self,direction):
+    def move_step(self, direction):
 
         self.move_rel(direction*self.stepsize)
         
     def recenter(self):
-        ''' Moves the stage to the center position'''
+        """Moves the stage to the center position."""
 
         self.move(50,unit = "u")
         self.position = 50E3
@@ -124,17 +122,17 @@ class Piezoconcept(si.SerialInstrument):
 
 PiezoStage = Piezoconcept(port = "COM9")
 
-#%% Let's play!
+#%% Testing:
 
 Piezoconcept.MOVEX(PiezoStage, 20, "u")
 
-#%% Let's play!
+#%%
 
 Piezoconcept.MOVEY(PiezoStage, 25, "u")
 
 #%%
 
-# Piezoconcept.INFOS(PiezoStage)
+#Piezoconcept.INFOS(PiezoStage)
 Piezoconcept.GET_X(PiezoStage)
 
 #%%
