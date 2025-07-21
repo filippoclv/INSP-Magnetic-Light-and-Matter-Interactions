c5e6e7ae185d8341578e95a06a3e6e1e7e40f6d0
Filippo Calavaro <calavaro@insp.jussieu.fr>
2025-07-21 18:25:11 +0200

    updates

--- ANPs/DataAnalysis/DataAnalysisNF.py
+++ ANPs/DataAnalysis/DataAnalysisNF.py
@@ -249,8 +249,66 @@ def plot_spectra_heights_norm(spectra_dict, integration_time, data, fig=None, ax
 
     return fig, ax
 
+def plot_all_power_curvesNF(datasets, int_start, int_end):
+
+    fig, ax = plt.subplots(figsize=(14, 12), constrained_layout=True)
+
+    # Use Z for color coding
+    z_values = np.array([data.get("Z_mV", 0) for data in datasets])
+    norm = plt.Normalize(vmin=-1000, vmax=0)
+    cmap = plt.cm.jet
+    colors = cmap(norm(z_values))
+
+    for i, data in enumerate(datasets):
+
+        folder = data["folder"]
+        int_time = data["integration_time"]
+        ratio_start = data["ratio_start"]
+        ratio_stop = data["ratio_stop"]
+
+        power_info_file = Path(folder) / "SetInfoPowerCurve.txt"
+        power_info = pd.read_csv(power_info_file, sep="\t")
+        power_map = dict(zip(power_info["Pindex"], power_info["CurrentPower"]))
+
+        all_spectra = read_all_spectra(folder)
+        results_df = integrate_peak(all_spectra, int_start, int_end, integration_time=int_time)
+
+        config_label = f"{data.get('label', ''):<6}"  # Empty if not present
+        #label = f"{config_label} | Int. time: {int_time:.1f} s | R: {ratio_start:.4f}–{ratio_stop:.2f}"
+
+        label = (
+                 f"Int. time: {int_time:>1.2f} s | "
+                 f"R: {ratio_start:>1.4f} – {ratio_stop:>1.3f} | "
+                )
+
+        ax.plot(
+          results_df["Power_W"],
+                results_df["Luminescence_counts"],
+                marker="o",
+                markersize=6,
+                markerfacecolor="none",
+                linestyle="-",
+                linewidth=2,
+                label=label,
+                color=colors[i]
+               )
+
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_title(f"log-log scale: luminescence vs power\n({int_start}–{int_end} nm peak)", fontsize=14)
+    ax.set_xlabel("Power [W]")
+    ax.set_ylabel("Luminescence [counts]")
+    ax.grid(True, which='both', linestyle='--', alpha=0.3)
+    ax.legend(fontsize=10, loc="best", prop={"family": "DejaVu Sans Mono"})
+
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    cbar = plt.colorbar(sm, ax=ax)
+    cbar.set_label("Z parameter [mV]", fontsize=12)
+
+    plt.show()
+
 def integrate_peakNF(spectra_dict, wl_min, wl_max, integration_time):
-    # Integrating with trapeizoidal method
 
     results = []
 
@@ -273,45 +331,43 @@ def integrate_peakNF(spectra_dict, wl_min, wl_max, integration_time):
     return pd.DataFrame(results, columns=["Height_mV", "Luminescence_counts", "Integrated_counts"])
 
 def integral_map_different_heights(spectra_dict, integration_time, wl_start, wl_stop, wl_step=1.0, ref_df=None):
+
     heights = []
     wl_centers = np.arange(wl_start, wl_stop, wl_step)
     data_matrix = []
 
     ref_integrated = None
+
     if ref_df is not None:
+
         ref_integrated = []
+
         for wl in wl_centers:
+
             ref_window = ref_df[(ref_df["Wavelength_nm"] >= wl) & (ref_df["Wavelength_nm"] < wl + wl_step)]
             val = np.trapz(ref_window["Intensity_counts"], ref_window["Wavelength_nm"]) / integration_time
             ref_integrated.append(val)
+
         ref_integrated = np.array(ref_integrated)
 
     for label, df in sorted(spectra_dict.items(), key=lambda x: x[1].attrs["Height"]):
+
         height = df.attrs["Height"]
         heights.append(height)
         intensities = []
 
         for wl in wl_centers:
+
             window = df[(df["Wavelength_nm"] >= wl) & (df["Wavelength_nm"] < wl + wl_step)]
             val = np.trapz(window["Intensity_counts"], window["Wavelength_nm"]) / integration_time
             intensities.append(val)
 
         intensities = np.array(intensities)
 
-        # if ref_integrated is not None:
-        #     # Replace values < 10 with NaN, else compute the ratio
-        #     with np.errstate(divide='ignore', invalid='ignore'):
-        #         intensities = np.where(intensities >= 10, intensities / ref_integrated, np.nan)
-
-        # if ref_integrated is not None:
-        #     with np.errstate(divide='ignore', invalid='ignore'):
-        #         ratio = intensities / ref_integrated
-        #         ratio[ratio <= 0.9] = np.nan  # Apply threshold to ratio
-        #         intensities = ratio
-
         if ref_integrated is not None:
-            min_ref_threshold = 100  # Adjust based on your data (e.g., 5-20 counts/s per bin, considering noise)
-            min_signal_threshold = 150  # New threshold for signal luminescence; adjust as needed
+
+            min_signal_threshold = 150
+
             with np.errstate(divide='ignore', invalid='ignore'):
                 ratio = intensities / ref_integrated
                 mask = (intensities >= min_signal_threshold)
@@ -321,4 +377,3 @@ def integral_map_different_heights(spectra_dict, integration_time, wl_start, wl_
         data_matrix.append(intensities)
 
     return np.array(heights), wl_centers + wl_step / 2, np.array(data_matrix)
-
