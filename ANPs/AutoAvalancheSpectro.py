50eaa89e790ea1428cc189b19dc629bb0ccb7400
PV-info <paul.vzqz11@gmail.com>
2026-05-06 13:54:06 +0200

    Update AutoAvalancheSpectro.py

--- ANPs/AutoAvalancheSpectro.py
+++ ANPs/AutoAvalancheSpectro.py
@@ -183,8 +183,148 @@ def FindAvalanche(
 
     return LumQueueSize
         
+def FindAvalanchePowscale(
+            SaveDataFolder,
+            Pmin,
+            Pmax,
+            Ratio,
+            integration_time = 3):
+    
+    print("Autodetection is running ...")
+    rough_step_gap = 1.1 #First measurement and gap between measurements
+    fine_step_ratio = 10
+    fine_step_gap = np.power(10, np.log10(rough_step_gap) / fine_step_ratio) #Resolution of the avalanche
+    PowerLimit = Pmax / 5 #Safety
+    LumQueueSize = int(2 * fine_step_ratio)
+    SlopeQueueSize = 3
+    
+    #Create the output folder and files
+    
+    FolderTimeName = time.strftime('%Y%m%d%H%M%S', time.gmtime())
+    MyDataFolder = SaveDataFolder + '\\' + FolderTimeName
+    os.makedirs(MyDataFolder)
+    
+    FileInfo = open(MyDataFolder + '\\' + 'SetInfoPowerCurve.txt', 'w')
+    FileInfo.write(f'Power\tCurrentPower\tTime\tLuminosity\tInt. time = {integration_time}\tP_min = {Pmin}\tP_max = {Pmax}\n')
+    FileInfo.close()
+    
+    print(f'New folder and file created in {MyDataFolder}')
+    
+    #Dark noise measurement (6 points)
+    Power = Pmin
+    noise_lum_list = np.zeros(6)
+    
+    for l in range(6):
+        SpectrumName = f"Dark{l}"
+        noise_lum_list[l] = GetPointSpectro(Power, SpectrumName, MyDataFolder)[1]
         
-LumQueueSize = FindAvalanche(
+    noise = [np.mean(noise_lum_list), np.max(noise_lum_list)]
+    print("\n\n\nNoise calibrated")
+    
+    #Avalanche find (noise exit)
+    InNoise = True
+    LumNoiseExit = [0, 0]
+    PowNoiseExit = [0, 0]
+    index = 5
+    
+    while InNoise and Power < PowerLimit:
+        index += 1
+        Power *= rough_step_gap
+        SpectrumName = f"Noise{index}"
+        pump, measure = GetPointSpectro(Power, SpectrumName, MyDataFolder)
+        
+        if (measure - noise[1] > noise[0]):
+            #Ensure true noise exit
+            LumNoiseExit[0] = measure
+            PowNoiseExit[0] = pump
+            SpectrumName = f"Try{index}"
+            pump, measure = GetPointSpectro(Power, SpectrumName, MyDataFolder)
+            if (measure - noise[1] > noise[0] * 2):
+                LumNoiseExit[1] = measure
+                PowNoiseExit[1] = pump
+                InNoise = False
+                print("\n\n\nNoise exit found")
+          
+    #Area scan -> looking for the avalanche slow-down
+    Searching = True
+
+    #Creation of the queue that computes the luminescence slope
+    LastNPump = np.zeros(LumQueueSize)
+    LastNLum = np.zeros(LumQueueSize)
+    
+    LastNPump[-2] = PowNoiseExit[0]
+    LastNPump[-1] = PowNoiseExit[1]
+    LastNLum[-2] = LumNoiseExit[0]
+    LastNLum[-1] = LumNoiseExit[1]    
+    for j in range(LumQueueSize - 2):
+        index += 1 
+        SpectrumName = f"Avalanche{index}"
+        LastNPump[j], LastNLum[j] = GetPointSpectro(Power * fine_step_gap ** (2 - LumQueueSize + j), SpectrumName, MyDataFolder)
+    
+    Power *= fine_step_gap #Highest power used in a measurement and last in queue
+
+    #Creation of the queue that computes the max avalanche region
+    SlopeQueue = np.ones(SlopeQueueSize) * (-1)
+    AllTimeMaxMinOfQueue = -1
+    MaxOfQueue = 0
+    MinOfQueue = -1
+    
+    Avalanche = (0, 0)
+    
+    #Computing the avalanche peak (max slope)
+    
+    #First slope
+    fit_params = linregress(np.log10(LastNPump), np.log10(LastNLum))
+    slope_ini = fit_params.slope
+    SlopeQueue[-1] = slope_ini
+    
+    #Loop
+    
+    while Power < PowerLimit: #TEMPORARY
+        index += 1 
+        SpectrumName = f"Avalanche{index}"
+        #Update power and the queue of measures then find the slope
+        Power *= fine_step_gap
+        for i in range(LumQueueSize - 1):
+            LastNLum[i] = LastNLum[i + 1]
+            LastNPump[i] = LastNPump[i + 1]
+        LastNPump[-1], LastNLum[-1] = GetPointSpectro(Power, SpectrumName, MyDataFolder)
+
+        fit_params = linregress(np.log10(LastNPump), np.log10(LastNLum))
+        slope_next = fit_params.slope
+        
+        
+        #Update the slope queue
+        for k in range(SlopeQueueSize - 1):
+            SlopeQueue[k] = SlopeQueue[k + 1]
+        SlopeQueue[-1] = slope_next
+        
+        MaxOfQueue = max(SlopeQueue)
+        MinOfQueue = min(SlopeQueue)
+        
+        if Searching and MinOfQueue > AllTimeMaxMinOfQueue:
+            AllTimeMaxMinOfQueue = MinOfQueue
+            #Mark the spot
+            adjust = (LumQueueSize - SlopeQueueSize) // 2 - LumQueueSize // 10 - 1
+            tempx, tempy = [LastNPump[adjust], LastNLum[adjust]]
+            Avalanche = tempx, tempy
+        elif not Searching:
+            Power *= rough_step_gap
+        
+        #Check if Max avalanche was reached
+        
+        if AllTimeMaxMinOfQueue > MaxOfQueue and Searching:
+            Searching = False
+            plt.plot(Avalanche[0], Avalanche[1], "bo")
+            print("\n\n\nAvalanche found")
+        slope_ini = np.copy(slope_next)
+    
+    fMoveToPower(RotorStage, PowerMeter, Pmin + Ratio * (Pmax - Pmin), *PowerRangeFitParameters)
+
+    return LumQueueSize
+        
+        
+LumQueueSize = FindAvalanchePowscale(
             SaveDataFolder,
             Pmin,
             Pmax,
@@ -194,6 +334,3 @@ plt.yscale("log")
 plt.xscale("log")
 plt.title("Acquired data{}".format(LumQueueSize))
 plt.show()
-
-    
-
